16683b6cb73e44bac9aaaab44e9ee9e7297a52ad
Kristiana12 <kristiana.1992@hotmail.com>
2022-08-02 17:20:14 +0200

    working on showing the word and error message

--- script.js
+++ script.js
@@ -25,17 +25,51 @@ const words = [
 
 const repeatingWords = [];
 
+// Choose a random Word from the array
 const chooseRandomWord = () => {
   const randomNum = Math.floor(Math.random() * words.length);
   repeatingWords.push(words[randomNum]);
-  console.log(words[randomNum]);
+  return words[randomNum];
 };
 
+const chosenRandomWord = chooseRandomWord();
+
+//Create the UI for the Word
 const createLetterEl = () => {
   const wordField = document.querySelector('.word-field');
-  wordField.appendChild(document.createElement('span'));
+  for (const letter of chosenRandomWord) {
+    const createListEl = wordField.appendChild(document.createElement('li'));
+    createListEl.classList.add('letter');
+    const createSpanEl = createListEl.appendChild(
+      document.createElement('span')
+    );
+    createSpanEl.innerHTML = letter;
+  }
 };
 
+//Show if the letter that was pressed is right
+document.addEventListener('keydown', (e) => {
+  const spanEL = document.querySelectorAll('.word-field span');
+  const heartIcons = document.querySelectorAll('.lifes svg');
+  const errorMessage = document.querySelector('.message-error');
+
+  for (let i = 0; i < spanEL.length; i++) {
+    if (spanEL[i].innerHTML === e.key) {
+      spanEL[i].classList.add('show');
+      errorMessage.classList.remove('show');
+    }
+  }
+
+  // for (const [index, span] of spanEL.entries()) {
+  //   console.log(span[index]);
+  //   // if (span[index].innerHTML === e.key) {
+  //   //   span.classList.add('show');
+  //   //   errorMessage.classList.remove('show');
+  //   // } else {
+  //   //   errorMessage.classList.add('show');
+  //   // }
+  // }
+});
+
 createLetterEl();
-chooseRandomWord();
 console.log(repeatingWords);
