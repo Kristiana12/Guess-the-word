def9ee3314933f577300aed75211e1847df691b7
Kristiana12 <kristiana.1992@hotmail.com>
2022-08-03 20:38:11 +0200

    fuctionality is implemented

--- script.js
+++ script.js
@@ -23,53 +23,80 @@ const words = [
   'deer',
 ];
 
-const repeatingWords = [];
+const wordLetters = [];
+const rightLettersUsed = new Set();
+let isPlaying;
 
 // Choose a random Word from the array
 const chooseRandomWord = () => {
+  isPlaying = true;
   const randomNum = Math.floor(Math.random() * words.length);
-  repeatingWords.push(words[randomNum]);
   return words[randomNum];
 };
 
 const chosenRandomWord = chooseRandomWord();
 
 //Create the UI for the Word
-const createLetterEl = () => {
+const createUnderscoresEl = () => {
   const wordField = document.querySelector('.word-field');
   for (const letter of chosenRandomWord) {
     const createListEl = wordField.appendChild(document.createElement('li'));
     createListEl.classList.add('letter');
-    const createSpanEl = createListEl.appendChild(
-      document.createElement('span')
-    );
-    createSpanEl.innerHTML = letter;
+    wordLetters.push(letter);
   }
+};
+
+createUnderscoresEl();
+const lettersUnique = new Set(wordLetters);
+// console.log(wordLetters);
+//Change heart colors when wrong
+const heartIcons = document.querySelectorAll('.lifes svg > path');
+let totalHeartnumbers = heartIcons.length - 1;
+
+const removeHearts = () => {
+  heartIcons[totalHeartnumbers].style.fill = '#393b45';
+  totalHeartnumbers--;
 };
 
 //Show if the letter that was pressed is right
 document.addEventListener('keydown', (e) => {
-  const spanEL = document.querySelectorAll('.word-field span');
-  const heartIcons = document.querySelectorAll('.lifes svg');
-  const errorMessage = document.querySelector('.message-error');
+  const listEL = document.querySelectorAll('.word-field li');
+  const errorMessage = document.querySelector('.message');
 
-  for (let i = 0; i < spanEL.length; i++) {
-    if (spanEL[i].innerHTML === e.key) {
-      spanEL[i].classList.add('show');
-      errorMessage.classList.remove('show');
+  let hasLetter = false;
+  //If word contains letter
+  if (isPlaying) {
+    errorMessage.classList.add('show');
+    if (wordLetters.join('').includes(e.key)) {
+      rightLettersUsed.add(e.key);
+      for (const [index, letter] of wordLetters.entries()) {
+        if (e.key === letter) {
+          listEL[index].innerHTML = e.key;
+          listEL[index].classList.add('show');
+          errorMessage.innerHTML = `Congrats letter ${e.key} is included!`;
+          hasLetter = true;
+        }
+      }
     }
   }
+  //If unique characters are equal to the used ones : WIN
+  if (rightLettersUsed.size === lettersUnique.size) {
+    errorMessage.innerHTML = `Congratulations you won!`;
+    errorMessage.classList.add('win');
+    isPlaying = false;
+  }
 
-  // for (const [index, span] of spanEL.entries()) {
-  //   console.log(span[index]);
-  //   // if (span[index].innerHTML === e.key) {
-  //   //   span.classList.add('show');
-  //   //   errorMessage.classList.remove('show');
-  //   // } else {
-  //   //   errorMessage.classList.add('show');
-  //   // }
-  // }
+  if (isPlaying) {
+    if (!hasLetter) {
+      errorMessage.innerHTML = `Oops the letter you chose is not included, guess again!`;
+      hasLetter = false;
+      removeHearts();
+    }
+  }
+  //Check if user lost the game
+  if (totalHeartnumbers < 0) {
+    errorMessage.innerHTML = `Sorry you lost!`;
+    errorMessage.classList.add('lose');
+    isPlaying = false;
+  }
 });
-
-createLetterEl();
-console.log(repeatingWords);
