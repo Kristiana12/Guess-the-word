9d045089e4d1cf859057bb8262226b4fc604ff0f
Kristiana12 <kristiana.1992@hotmail.com>
2022-08-07 09:42:20 +0200

    before the reset btn

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ const wordLetters = [];
 const rightLettersUsed = new Set();
 let isPlaying;
 
+const wordField = document.querySelector('.word-field');
+const resetBtn = document.querySelector('.reset');
+
 // Choose a random Word from the array
 const chooseRandomWord = () => {
   isPlaying = true;
@@ -34,11 +37,12 @@ const chooseRandomWord = () => {
   return words[randomNum];
 };
 
-const chosenRandomWord = chooseRandomWord();
+let chosenRandomWord = chooseRandomWord();
+
+const init = () => {};
 
 //Create the UI for the Word
 const createUnderscoresEl = () => {
-  const wordField = document.querySelector('.word-field');
   for (const letter of chosenRandomWord) {
     const createListEl = wordField.appendChild(document.createElement('li'));
     createListEl.classList.add('letter');
@@ -61,19 +65,19 @@ const removeHearts = () => {
 //Show if the letter that was pressed is right
 document.addEventListener('keydown', (e) => {
   const listEL = document.querySelectorAll('.word-field li');
-  const errorMessage = document.querySelector('.message');
+  const message = document.querySelector('.message');
 
   let hasLetter = false;
   //If word contains letter
   if (isPlaying) {
-    errorMessage.classList.add('show');
+    message.classList.add('show');
     if (wordLetters.join('').includes(e.key)) {
       rightLettersUsed.add(e.key);
       for (const [index, letter] of wordLetters.entries()) {
         if (e.key === letter) {
           listEL[index].innerHTML = e.key;
           listEL[index].classList.add('show');
-          errorMessage.innerHTML = `Congrats letter ${e.key} is included!`;
+          message.innerHTML = `Congrats letter "${e.key}" is included!`;
           hasLetter = true;
         }
       }
@@ -81,22 +85,24 @@ document.addEventListener('keydown', (e) => {
   }
   //If unique characters are equal to the used ones : WIN
   if (rightLettersUsed.size === lettersUnique.size) {
-    errorMessage.innerHTML = `Congratulations you won!`;
-    errorMessage.classList.add('win');
+    message.innerHTML = `Congratulations you won!`;
+    message.classList.add('win');
     isPlaying = false;
   }
 
   if (isPlaying) {
     if (!hasLetter) {
-      errorMessage.innerHTML = `Oops the letter you chose is not included, guess again!`;
+      message.innerHTML = `Oops the letter "${e.key}" is not included, guess again!`;
       hasLetter = false;
       removeHearts();
     }
   }
   //Check if user lost the game
   if (totalHeartnumbers < 0) {
-    errorMessage.innerHTML = `Sorry you lost!`;
-    errorMessage.classList.add('lose');
+    message.innerHTML = `Sorry you lost!`;
+    message.classList.add('lose');
     isPlaying = false;
   }
 });
+
+resetBtn.addEventListener('click', init);
